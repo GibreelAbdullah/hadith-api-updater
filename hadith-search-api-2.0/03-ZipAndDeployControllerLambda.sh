f4dea308a35c9125e6b22240c25e2ea43fdb807a
Gibreel Abdullah <gibreel.khan@gmail.com>
2024-08-16 11:42:46 +0530

    Reordered because of issue

--- hadith-search-api-2.0/03-ZipAndDeployControllerLambda.sh
+++ hadith-search-api-2.0/03-ZipAndDeployControllerLambda.sh
@@ -11,14 +11,16 @@ FUNCTION_NAME="HadithSearchController"
 
 if aws lambda get-function --function-name "$FUNCTION_NAME" > /dev/null 2>&1; then
     echo "Function $FUNCTION_NAME already exists. Updating it."
-    aws lambda update-function-code \
-        --function-name "$FUNCTION_NAME" \
-        --zip-file fileb://controller.zip \
-        --no-cli-pager
     aws lambda update-function-configuration \
         --function-name "$FUNCTION_NAME" \
         --environment Variables="{FUNCTION_NAME=\"${WORKER_FUNCTION_NAMES_LIST::-1}\"}" \
         --no-cli-pager
+    
+    aws lambda update-function-code \
+        --function-name "$FUNCTION_NAME" \
+        --zip-file fileb://controller.zip \
+        --no-cli-pager
+
 else
     echo "Creating function $FUNCTION_NAME."
     aws lambda create-function \
